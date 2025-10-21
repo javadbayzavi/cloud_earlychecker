86e0ff4fab6a14c1ccbac923d124056594846868
Javad Bayzavi <javadbayzavi@gmail.com>
2025-10-21 18:20:59 +0200

    remove unnecessary exceptions

--- src/aws_earlychecker/exception.py
+++ src/aws_earlychecker/exception.py
@@ -1,22 +1,6 @@
-import typer
-class InvalidCommandError(Exception):
-    """Base exception for invalid CLI commands."""
-    pass
-
-
 class AWSHealthCheckError(Exception):
     """Custom exception for AWS health check errors."""
+    def __init__(self, message: str):
+        super().__init__(message)
     pass
-
-
-class EmptyCommandError(InvalidCommandError):
-    """Raised when no CLI command is entered."""
-    def __init__(self):
-        super().__init__("No command entered. Please provide a valid command. Use --help for a list of available commands.")
-
-
-class NoSuchCommandError(InvalidCommandError):
-    """Raised when a non-existent command is entered."""
-    def __init__(self, command: str):
-        super().__init__(f"The command '{command}' does not exist. Use --help for a list of available commands.")
 

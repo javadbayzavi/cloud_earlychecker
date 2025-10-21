7402302f2cc8921aeb6c8764b41498887bc3229f
Javad Bayzavi <javadbayzavi@gmail.com>
2025-10-21 17:29:40 +0200

    make modules accessible

---
+++ src/aws_earlychecker/main.py
@@ -0,0 +1,10 @@
+from aws_earlychecker.cli import AWSEarlyCheckerCLI
+from aws_earlychecker.core.app_runner import AppRunner
+
+if __name__ == "__main__":
+    #Initialize the CLI application with callback and exception handler
+    callback = AWSEarlyCheckerCLI.get_app_callback()
+    exception_handler = AWSEarlyCheckerCLI.get_exception_handler()
+
+    runner = AppRunner(AWSEarlyCheckerCLI)
+    runner.run(callback=callback, exception_handler=exception_handler)

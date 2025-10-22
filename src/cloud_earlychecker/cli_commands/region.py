4e67d2b6e67de38d9a854b3f6eaefb459ced8665
Javad Bayzavi <javadbayzavi@gmail.com>
2025-10-22 19:30:49 +0200

    Region persist default aws region in the app state

--- src/cloud_earlychecker/cli_commands/region.py
+++ src/cloud_earlychecker/cli_commands/region.py
@@ -1,8 +1,20 @@
 import typer
+from rich import print
 
-def region(region: str = typer.Argument(..., help="AWS region to set as default, e.g., us-east-1")):
+from cloud_earlychecker.core.app_state import AppState
+
+def aws_region(ctx: typer.Context, region: str = typer.Argument(..., help="AWS region to set as default, e.g., us-east-1")):
     """
     Set default AWS region.
     """
+    app_state = AppState.get_app_state(ctx)
+    
+    app_region = app_state.get("AWS_DEFAULT_REGION")
+
+    if app_region:
+        print(f"[yellow]Warning:[/yellow] Overriding machine default region: [bold]{app_region}[/bold]")
+        
+    app_state.set("AWS_DEFAULT_REGION", region)
+
     # Placeholder for setting region logic
     print(f"Default AWS region set to: [bold]{region}[/bold]")

94b22f1db0c64409f7a5cf12ec4f63d4f95a5b37
Javad Bayzavi <javadbayzavi@gmail.com>
2025-10-22 17:55:43 +0200

    version is read from env

--- src/cloud_earlychecker/cli_commands/version.py
+++ src/cloud_earlychecker/cli_commands/version.py
@@ -1,7 +1,17 @@
 from rich import print
-def version():
+import typer
+
+def version(ctx: typer.Context):
     """
     Display version information.
     """
-    from cloud_earlychecker import __version__
-    print(f"AWS EarlyCheck version: [bold]{__version__.__version__}[/bold]")
+    if ctx.obj:
+        app_state = ctx.obj
+        environment = app_state.getEnvironment()
+        version_info = environment.get_from_environment("VERSION")
+        if version_info:
+            print(f"AWS EarlyCheck version from environment: [bold]{version_info}[/bold]")
+            return
+        else:
+            from cloud_earlychecker import __version__
+            print(f"AWS EarlyCheck version: [bold]{__version__.__version__}[/bold]")

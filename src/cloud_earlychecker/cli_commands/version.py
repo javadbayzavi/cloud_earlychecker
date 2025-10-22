33843c5b12dc2d51350b225ed464e00d59b102f6
Javad Bayzavi <javadbayzavi@gmail.com>
2025-10-22 13:03:58 +0200

    Small work on version command

--- src/cloud_earlychecker/cli_commands/version.py
+++ src/cloud_earlychecker/cli_commands/version.py
@@ -1,6 +1,7 @@
+from rich import print
 def version():
     """
     Display version information.
     """
     from cloud_earlychecker import __version__
-    print(f"AWS EarlyCheck version: [bold]{__version__}[/bold]")
+    print(f"AWS EarlyCheck version: [bold]{__version__.__version__}[/bold]")
